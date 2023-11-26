8c3876625e7e5786e0d9218884e64755e74de636
Antiquete <antiquete@proton.me>
2023-11-26 07:06:51 +0530

    drivers: gpu: Fix more missing prototypes
    Signed-off-by: Antiquete <antiquete@proton.me>

--- drivers/gpu/drm/mediatek/mtk_drm_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_drm_crtc.c
@@ -7054,7 +7054,7 @@ void mtk_wait_framedone(int fd_num) {
 	}
 }
 
-void hbm_notify_fingerprint_if_neccessary_vdo() {
+void hbm_notify_fingerprint_if_neccessary_vdo(void) {
 	if (framedone_remain == 0) return;
 
 	framedone_remain --;
